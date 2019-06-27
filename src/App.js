f1b2dd56be1e789371a82b653c4f618f5b4a9c1b
cs-memorang <chudeep@memorangapp.com>
2019-06-27 12:35:25 +0530

    Add redux to project

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, { Component } from "react";
 import HelloWorld from "./HelloWorld";
 
 class App extends Component {
-  state = {
-    tech: "React"
-  };
   render() {
     return <HelloWorld tech={this.state.tech} />;
   }
